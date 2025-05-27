3a8cb930acd78a0aad9fbde543f3753ca5ef0037
¨jeandavidvasquez¨ <jeandavidvasquezyaruqui@gmail.com>
2025-05-27 09:39:36 -0500

    3

--- consulta3.py
+++ consulta3.py
@@ -14,8 +14,8 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 nombres_estudiantes = ["Jennifer Bolton", "Elaine Perez", "Heather Henderson", "Charles Harris"]
-
+# consulto los objetos de tipo tarea luego lo uno con la tabla de entrega,estudiante y filtro los nombres con un in que me sirve para poner el arreglo de estudaintes que cree previamente.
 tareas = session.query(Tarea).join(Entrega).join(Estudiante).filter(Estudiante.nombre.in_(nombres_estudiantes)).all()
-
+#con el for presente los datos
 for tarea in tareas:
     print(f"Tarea: {tarea.titulo} -> Número de entregas: {len(tarea.entregas)}")
